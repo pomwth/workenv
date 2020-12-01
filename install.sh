bb993a463498c200a3ab4fe9c2527e3dfc55df63
Wallop Th <pom.wallop.th@gmail.com>
2020-12-02 01:23:14 +0700

    feat(install): Initial commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt install git -y
+git config --global user.email "pom.wallop.th@gmail.com"
+git config --global user.name "Wallop Th"
