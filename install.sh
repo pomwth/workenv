b5dd3a574cc96c2f49f2d4edd03c7175879b989e
Wallop Th <pom.wallop.th@gmail.com>
2020-12-03 01:32:42 +0700

    feat(install): Install oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+DIR=`dirname "$0"`
+UNIQID=$(date+'%Y%m%d%H%M%S')
+
 sudo apt update
 sudo apt upgrade
 sudo apt-get update
 sudo apt-get upgrade
+
+sudo apt install mlocate -y
+sudo apt install curl -y
+
+#https://brew.sh/
+#for Linux - install Homebrew
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+#test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+#test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+#echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
+sudo apt install zsh -y
+sudo apt install powerline fonts-powerline -y
+mv ~/.oh-my-zsh ~/.oh-my-zsh-BCK-$UNIQID
+ln -s $DIR/oh-my-zsh ~/.oh-my-zsh
+rm -f ~/.zshrc
+mv ~/.zshrc ~/.zshrc-BCK-$UNIQID
+ln -s $DIR/oh-my-zsh/.zshrc ~/.zshrc
 
 sudo apt install git -y
 git config --global user.email "pom.wallop.th@gmail.com"
---
+++ test.sh
@@ -0,0 +1,7 @@
+ID=$(date +'%Y%m%d%H%M%S')
+
+echo $(pwd)
+echo `dirname "$0"`
+echo $(date +'%y-%m-%d %H:%M:%S')
+echo $(date +'%N')
+echo .zshrc-bck-$ID
