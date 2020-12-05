2ce548814555dcbdc89775d09b886e0c9a195833
Wallop Th <pom.wallop.th@gmail.com>
2020-12-05 17:59:06 +0700

    feat(install): Install GitHub CLI

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR=`dirname "$0"`
-UNIQID=$(date+'%Y%m%d%H%M%S')
+UNIQID=$(date +'%Y%m%d%H%M%S')
 
 sudo apt update
 sudo apt upgrade
@@ -23,10 +23,19 @@ sudo apt install zsh -y
 sudo apt install powerline fonts-powerline -y
 mv ~/.oh-my-zsh ~/.oh-my-zsh-BCK-$UNIQID
 ln -s $DIR/oh-my-zsh ~/.oh-my-zsh
-rm -f ~/.zshrc
-mv ~/.zshrc ~/.zshrc-BCK-$UNIQID
-ln -s $DIR/oh-my-zsh/.zshrc ~/.zshrc
+#rm -f ~/.zshrc
+#mv ~/.zshrc ~/.zshrc-BCK-$UNIQID
+#ln -s $DIR/oh-my-zsh/.zshrc ~/.zshrc
 
+# Install GIT
 sudo apt install git -y
 git config --global user.email "pom.wallop.th@gmail.com"
 git config --global user.name "Wallop Th"
+
+# Install GitHub CLI
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh
+
+
