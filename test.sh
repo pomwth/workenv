0659021c306ecd410566fed7e919c515ff9b066d
Wallop Th <pom.wallop.th@gmail.com>
2020-12-06 22:57:35 +0700

    Update sh variables.

--- test.sh
+++ test.sh
@@ -1,7 +1,15 @@
 ID=$(date +'%Y%m%d%H%M%S')
 
-echo $(pwd)
-echo `dirname "$0"`
-echo $(date +'%y-%m-%d %H:%M:%S')
-echo $(date +'%N')
-echo .zshrc-bck-$ID
+#echo $(pwd)
+#echo `dirname "$0"`
+#echo `dirname "$1"`
+#echo $(date +'%y-%m-%d %H:%M:%S')
+#echo $(date +'%N')
+#echo .zshrc-bck-$ID
+
+#!/bin/bash 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo $SCRIPTPATH
